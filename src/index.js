8c95ec5e3cc3abdc25a34d1025e17fb0efe61e90
Vincde <145802178+Vincde@users.noreply.github.com>
2024-10-09 18:40:26 +0200

    Substitute shipConstructor code from index to the corresponding file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,2 @@
 import "./style.css";
-
-class Ship {
-  constructor(length, numberOfTimesHit, isSunk) {
-    this.length = length;
-    this.numberOfTimesHit = numberOfTimesHit;
-    this.isSunk = isSunk;
-  }
-
-  hit() {
-    this.numberOfTimesHit += 1;
-  }
-
-  isSunk() {
-    if (this.numberOfTimesHit >= this.length) {
-      return true;
-    }
-    return false;
-  }
-}
+import Ship from "./mainConstructor/shipConstructor";
---
+++ src/mainConstructor/shipConstructor.js
@@ -0,0 +1,20 @@
+class Ship {
+  constructor(length, numberOfTimesHit, isSunk) {
+    this.length = length;
+    this.numberOfTimesHit = numberOfTimesHit;
+    this.isSunk = isSunk;
+  }
+
+  hit() {
+    this.numberOfTimesHit += 1;
+  }
+
+  isSunk() {
+    if (this.numberOfTimesHit >= this.length) {
+      return true;
+    }
+    return false;
+  }
+}
+
+export default Ship;
