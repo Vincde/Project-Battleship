9ee42339e4ea5f120347fcdd5d98d06bc5a35cef
Vincde <145802178+Vincde@users.noreply.github.com>
2024-10-09 18:26:18 +0200

    Create folder mainConstructor, with shipConstructor file inside.
    Inside the shipConstructor folder there is the starting Ship class, with its properties length, numberOfTimesHit and isSunk. There has been added hit() and isSunk(). One for adding one to hits and the other to verify if a ship should be destroyed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+class Ship {
+  constructor(length, numberOfTimesHit, isSunk) {
+    this.length = length;
+    this.numberOfTimesHit = numberOfTimesHit;
+    this.isSunk = isSunk;
+  }
+
+  hit() {
+    this.numberOfTimesHit += 1;
+  }
+
+  isSunk() {
+    if (this.numberOfTimesHit >= this.length) {
+      return true;
+    }
+    return false;
+  }
+}
