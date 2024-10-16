2294d6242c409a67b5a6dbff3e7dc970b297b155
Vincde <145802178+Vincde@users.noreply.github.com>
2024-10-16 19:16:07 +0200

    Add starting game, that creates two players and manually adds ships into their gameBoard objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./style.css";
+// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
+import Player from "./playerConstructor/players";
+
+function newGame() {
+  const player1 = new Player("Marco");
+  const player2 = new Player("Antonio");
+
+  player1.gameBoard.placeShip(2, 2, 7);
+  player1.gameBoard.placeShip(2, 2, 1);
+  player1.gameBoard.placeShip(2, 5, 7);
+  player1.gameBoard.placeShip(2, 3, 3);
+  player1.gameBoard.placeShip(2, 8, 1);
+  player1.gameBoard.placeShip(2, 5, 7);
+
+  player2.gameBoard.placeShip(2, 2, 7);
+  player2.gameBoard.placeShip(2, 2, 1);
+  player2.gameBoard.placeShip(2, 5, 7);
+  player2.gameBoard.placeShip(2, 3, 3);
+  player2.gameBoard.placeShip(2, 8, 1);
+  player2.gameBoard.placeShip(2, 5, 7);
+}
+
+newGame();
