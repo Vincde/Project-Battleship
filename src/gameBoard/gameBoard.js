0782ffcca84227334bdf0950c920fb990f5c5c1c
Vincde <145802178+Vincde@users.noreply.github.com>
2025-01-08 11:38:14 +0100

    Add placeShip to object gameBoard
    The object needs to contain this function, and it needs to return the array values to be occupied by the ships. This prevents the need to add two types of representation into the board

--- src/gameBoard/gameBoard.js
+++ src/gameBoard/gameBoard.js
@@ -1,5 +1,30 @@
 function gameBoard() {
-  function placeShip(x, y) {}
+  function placeShip(x, y, length, direction) {
+    let count = 0;
+    const returningPos = [];
+    for (let i = 0; i < x; i += 1) {
+      for (let j = 0; j < 10; j += 1) {
+        count += 1;
+      }
+    }
+    count += y - 1;
+
+    if (direction === "horizontal") {
+      for (let i = 0; i < length; i += 1) {
+        returningPos.push(count);
+        count += 1;
+      }
+    } else if (direction === "vertical") {
+      for (let w = 0; w < length; w += 1) {
+        returningPos.push(count);
+        count += 10;
+      }
+    }
+
+    count = 0;
+
+    return returningPos;
+  }
 
   return { placeShip };
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,1 +1,10 @@
+import gameBoard from "./gameBoard/gameBoard";
 
+test("placeShip function works correctly", () => {
+  const GB = gameBoard();
+  let result2 = [];
+
+  result2 = GB.placeShip(4, 5, 3, "vertical");
+
+  expect(result2).toEqual([44, 54, 64]);
+});
