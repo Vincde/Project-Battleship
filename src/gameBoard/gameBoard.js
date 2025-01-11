0b75a65ea4d8ccad13c12073e7b96d8ae8d637ee
Vincde <145802178+Vincde@users.noreply.github.com>
2025-01-11 13:53:27 +0100

    Add verifyData function into gameBoard function object
    The function verifyData takes coordinates,length and direction and verifies these things:
    - if coordinates and length are correct
    - if direction is spelled correctly
    - based on the direction it verifies if there are already placed ships

--- src/gameBoard/gameBoard.js
+++ src/gameBoard/gameBoard.js
@@ -3,19 +3,57 @@ import Ship from "../ship/ship";
 function gameBoard() {
   const gameBoardTable = [[], [], [], [], [], [], [], [], [], []];
 
-  function placeShip(x, y, length, direction) {
-    const newShip = Ship();
-    gameBoardTable[x][y] = newShip;
+  function verifyData(x, y, length, direction) {
+    // i need to verify if all the data are correct
+    // and don't go into a not acceptable state
 
-    if (direction === "vertical") {
-      for (let i = x + 1; i < length + x; i += 1) {
-        gameBoardTable[i][y] = newShip;
+    if (x > 9 || x < 0 || y > 9 || y < 0 || length < 0 || length > 5) {
+      // i need a method to verify if the inserted data are incorrect
+      return false;
+    }
+
+    if (direction !== "horizontal" && direction !== "vertical") {
+      // verify if direction is correct
+      // (theorically this shouldn't be added as most probably there will be only 2 options)
+      return false;
+    }
+
+    if (direction === "horizontal") {
+      if (length + y - 1 > 9) return false;
+
+      for (let i = y; i < y + length - 1; i += 1) {
+        if (typeof gameBoardTable[x][i] === "object") return false;
       }
-    } else if (direction === "horizontal") {
-      for (let j = y + 1; j < length + y; j += 1) {
-        gameBoardTable[x][j] = newShip;
+    } else if (direction === "vertical") {
+      if (length + x - 1 > 9) return false;
+
+      for (let i = x; i < x + length - 1; i += 1) {
+        if (typeof gameBoardTable[i][y] === "object") return false;
       }
     }
+
+    return true;
+  }
+
+  function placeShip(x, y, length, direction) {
+    if (verifyData(x, y, length, direction) === true) {
+      const newShip = Ship();
+      gameBoardTable[x][y] = newShip;
+
+      if (direction === "vertical") {
+        for (let i = x + 1; i < length + x; i += 1) {
+          gameBoardTable[i][y] = newShip;
+        }
+      } else if (direction === "horizontal") {
+        for (let j = y + 1; j < length + y; j += 1) {
+          gameBoardTable[x][j] = newShip;
+        }
+      }
+    } else {
+      return false;
+    }
+
+    return true;
   }
 
   function getShip(x, y) {
