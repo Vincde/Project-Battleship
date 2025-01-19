243c024ab59b9c0749697c15f03538ca27c93d98
Vincde <145802178+Vincde@users.noreply.github.com>
2025-01-19 22:27:39 +0100

    //

--- src/index.js
+++ src/index.js
@@ -5,20 +5,12 @@ import UI from "./userInterface/UI";
 function main() {
   const userInterface = UI();
   userInterface.paintBoards();
-  /* const finishPositioningShips = document.querySelector(".startGame-button"); */
 
   const player1 = Player("Antonio");
   const player2 = Player("franco");
 
   player1.playerGameBoard.placeShip(0, 2, 3, "horizontal");
-  player1.playerGameBoard.placeShip(1, 2, 3, "horizontal");
-  player1.playerGameBoard.placeShip(3, 2, 3, "horizontal");
-  player1.playerGameBoard.placeShip(2, 2, 3, "horizontal");
-
-  player2.playerGameBoard.placeShip(0, 2, 3, "vertical");
-  player2.playerGameBoard.placeShip(0, 3, 3, "vertical");
-  player2.playerGameBoard.placeShip(0, 4, 3, "vertical");
-  player2.playerGameBoard.placeShip(0, 5, 3, "vertical");
+  player2.playerGameBoard.placeShip(0, 2, 3, "horizontal");
 
   userInterface.initiateBox(
     player1.playerGameBoard,
