cd102e0c02884271b6a54f51cba7baa10dbae829
Vincde <145802178+Vincde@users.noreply.github.com>
2025-10-09 12:46:40 +0200

    Initial test for placeShip (to be changed)

--- src/gameBoard/gameBoard.test.js
+++ src/gameBoard/gameBoard.test.js
@@ -2,6 +2,13 @@ import gameBoard from "./gameBoard";
 
 test("placeShip saves the correct result", () => {
   const newG = gameBoard();
+  jest.mock("./../ship/ship", () => {
+    return jest.fn((length) => ({
+      hit: jest.fn(),
+      isSunk: jest.fn().mockReturnValue(false),
+      getLength: jest.fn().mockReturnValue(3),
+    }));
+  });
 
   newG.placeShip(2, 3, 3, "v");
   expect(newG.getBoardElement(2, 3)).toContain({ row });
