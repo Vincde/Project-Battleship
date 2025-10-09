20c0c7fa1fdf25e3ac4611ecfd79cb1ec66ea989
Vincde <145802178+Vincde@users.noreply.github.com>
2025-10-09 15:35:45 +0200

    Fix: complete gameBoard object testing

--- src/gameBoard/gameBoard.test.js
+++ src/gameBoard/gameBoard.test.js
@@ -1,17 +1,23 @@
 import gameBoard from "./gameBoard";
+import ship from "../ship/ship";
+
+jest.mock("./../ship/ship");
 
 test("placeShip saves the correct result", () => {
   const newG = gameBoard();
-  jest.mock("./../ship/ship", () => {
-    return jest.fn((length) => ({
-      hit: jest.fn(),
-      isSunk: jest.fn().mockReturnValue(false),
-      getLength: jest.fn().mockReturnValue(3),
-    }));
-  });
+
+  const mockShip = { hit: jest.fn(), isSunk: jest.fn(), getLength: jest.fn() };
+  ship.mockReturnValue(mockShip);
 
   newG.placeShip(2, 3, 3, "v");
-  expect(newG.getBoardElement(2, 3)).toContain({ row });
 
-  expect(newG.getBoardElement(2, 3)).toContain({ column });
+  expect(typeof newG.getBoardElement(2, 3)).toBe("object");
+  expect(ship).toHaveBeenCalledWith(3);
+
+  expect(newG.getBoardElement(2, 3)).toEqual({
+    row: 2,
+    column: 3,
+    ship: mockShip,
+    direction: "v",
+  });
 });
