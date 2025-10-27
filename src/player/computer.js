aea23b222a2dcefb480e74cf3ce7fedc9b7bde9f
Vincde <145802178+Vincde@users.noreply.github.com>
2025-10-27 16:16:52 +0100

    Add name to computer function

--- src/player/computer.js
+++ src/player/computer.js
@@ -1,9 +1,11 @@
 import gameBoard from "../gameBoard/gameBoard";
+import player from "./player";
 
 function computer() {
   const playerGameBoard = gameBoard();
+  const playerName = null;
 
-  return { playerGameBoard };
+  return { playerGameBoard, playerName };
 }
 
 export default computer;
